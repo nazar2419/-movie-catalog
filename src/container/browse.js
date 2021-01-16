11d086cb9faf3f292d4cf2ab1a0fd72080dc983f
nazar2419 <nazarx095@gmail.com>
2021-01-17 01:36:41 +0200

    full project

--- src/container/browse.js
+++ src/container/browse.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import {SelectProfileContainer } from './profile';
+import { FooterContainer } from './footter';
 import { FirebaseContext } from '../context/firebase';
-import { Card, Header, Loading } from '../components';
+import { Card, Header, Loading, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.jpg';
 
@@ -26,6 +28,19 @@ export  function BrowseContainer({ slides }) {
     useEffect(() => {
         setSlideRows(slides[category]);
     }, [slides, category]);
+
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {
+            keys: ['data.description', 'data.title', 'data.genre'],
+        });
+        const results = fuse.search(searchTerm).map(({item}) => item);
+
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0 ) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+    }, [searchTerm])
 
     return profile.displayName ? (
         <>
@@ -96,12 +111,16 @@ export  function BrowseContainer({ slides }) {
                 </Card.Item>
               ))}
             </Card.Entities>
-                        <Card.Feature category={category}>
-                            <p>Hello</p>
-                        </Card.Feature>
-                    </Card>
+            <Card.Feature category={category}>
+                <Player>
+                    <Player.Button />
+                    <Player.Video src="/videos/bunny.mp4" />
+                </Player>
+            </Card.Feature>
+        </Card>
                 ))}
         </Card.Group>
+        <FooterContainer />
         </>
     ) : (
         <SelectProfileContainer user={user} setProfile={setProfile}  />
